b9d1b88f2cee25ece85e0abcad81fd0cf9b71de2
Raphaël Benzazon <r.benzazon@webpublication.fr>
2019-09-04 15:48:02 +0300

    cleaned JS updated npm

--- src/DonutTrio.js
+++ src/DonutTrio.js
@@ -1,5 +1,17 @@
 import * as d3 from "d3";
 
+/**
+ * TODOS :
+ * modularize
+ * optimize dom
+ * optimize d3 usage
+ * make the component updatable
+ * add init / update transition
+ * throw error when something fails
+ * make the component responsive
+ * omptimize d3 import
+ */
+
 export class DonutTrio extends HTMLElement {
   constructor() {
     super();
@@ -7,6 +19,24 @@ export class DonutTrio extends HTMLElement {
 
   //gets interpolated when adding to dom to inject external data
   getTemplate(values){
+
+    const {center:{
+        label:centerLabel,
+        value:centerValue
+      },
+      left:{
+        label:leftLabel,
+        labelColor:leftLabelColor,
+        percent:leftPercent,
+        value:leftValue,
+      },
+      right:{
+        label:rightLabel,
+        labelColor:rightLabelColor,
+        percent:rightPercent,
+        value:rightValue,
+      }
+    } = values;
     return `
     <style>
       .donut{
@@ -57,10 +87,10 @@ export class DonutTrio extends HTMLElement {
         left: 0;
       }
       .leftFigure>.label{
-        color: ${values.leftLabelColor};
+        color: ${leftLabelColor};
       }
       .rightFigure>.label{
-        color: ${values.rightLabelColor};
+        color: ${rightLabelColor};
       }
       .label{
         text-transform: capitalize;
@@ -91,20 +121,20 @@ export class DonutTrio extends HTMLElement {
         <div id="tickMarks" ></div>
         <div id="areaGraph" ></div>
         <div class="centerFigureText">
-          <span class="centerLabel">${values.centerLabelText}</span><br>
-          <span class="centerValue">${values.centerValueText}</span>
+          <span class="centerLabel">${centerLabel}</span><br>
+          <span class="centerValue">${centerValue}</span>
         </div>
       </div>
       <div class="bottomFigures">
         <div class="leftFigure">
-          <span class="label">${values.leftLabelText}</span><br>
-          <span class="percent">${values.leftPercentText}</span>
-          <span class="value">${values.leftValueText}</span>
+          <span class="label">${leftLabel}</span><br>
+          <span class="percent">${leftPercent}</span>
+          <span class="value">${leftValue}</span>
         </div>
         <div class="rightFigure">
-          <span class="label">${values.rightLabelText}</span><br>
-          <span class="percent">${values.rightPercentText}</span>
-          <span class="value">${values.rightValueText}</span>
+          <span class="label">${rightLabel}</span><br>
+          <span class="percent">${rightPercent}</span>
+          <span class="value">${rightValue}</span>
         </div>
       </div>
     </div>
@@ -112,61 +142,40 @@ export class DonutTrio extends HTMLElement {
   }
 
   appendTemplate(data){
-    
     const template = document.createElement('template');
-    template.innerHTML = this.getTemplate({
-      centerLabelText:data.center.label,
-      centerValueText:data.center.value,
-      leftLabelText:data.left.label,
-      leftLabelColor:data.left.labelColor,
-      leftPercentText:data.left.percent,
-      leftValueText:data.left.value,
-      rightLabelText:data.right.label,
-      rightLabelColor:data.right.labelColor,
-      rightPercentText:data.right.percent,
-      rightValueText:data.right.value,
-    });
+    template.innerHTML = this.getTemplate(data);
     this._shadow.appendChild(template.content.cloneNode(true));
   }
 
   //public setter to provide data to this component
   set data(value){
-    if(this._data !== undefined) return;
-    console.log("data"+JSON.stringify(value));
+    if(this._data !== undefined) return;//remove this when component handles update
     // Creates a shadow root
     this._shadow = this.attachShadow({mode: 'open'});
     if(value.center && value.left && value.right){
       this._data = value;
       this.appendTemplate(this._data);
-      this.drawGraphs(this._shadow);
+      this.drawGraphs(this._shadow,this._data,);
+    }else{
+      throw Error("ERROR - DonutTrio - malformed data");
     }
   }
 
-  //draws graphs using d3 library
-  drawGraphs(shadow){
-    /*
-    Initial SVG values
-    */
-    var width = 154;
-    var height = 154;
-    var margin = 10;
-    var radius = 77;
-    var svg;
-
-    var percentScale = d3.scaleLinear()
-        .domain([0, 100])
-        .range([0, 2 * Math.PI]);
-
-    /*
-    Donut Chart
-    */
+  /*
+  Donut Chart
+  */
+  drawDonutGraph(element,inputData,width,height){
     
-    svg = d3.select(shadow.querySelector("#donutGraph"))
-        .append("svg")
-        .attr("width", width)
-        .attr("height", height)
-        .append("g")
-        .attr("transform","translate(" + width/2 +","+ height/2 +")");
+    const percentScale = d3.scaleLinear()
+      .domain([0, 100])
+      .range([0, 2 * Math.PI]);
+
+    const svg = d3.select(element)
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .append("g")
+      .attr("transform","translate(" + width/2 +","+ height/2 +")");
 
     /*
     transforms following incoming json data
@@ -175,64 +184,76 @@ export class DonutTrio extends HTMLElement {
     [[0,40,"#507439"],[40,100,"#92d657"]]
     */
     let currentValue = 0;
-    let donutData = [];
-    for(let i=0;i<this._data.center.donut.length;i++){
-      donutData.push([
+    const donutData = inputData.map((item)=>{
+      let newItem = [
         currentValue,
-        currentValue+this._data.center.donut[i].value,
-        this._data.center.donut[i].color
-      ]);
-      currentValue = this._data.center.donut[i].value;
-    }
+        currentValue+item.value,
+        item.color
+      ]
+      currentValue = item.value;
+      return newItem;
+    });
 
-    var arc = d3.arc()
-        .innerRadius(71)
-        .outerRadius(77)
-        .startAngle((d) => percentScale(d[0]))
-        .endAngle((d) => percentScale(d[1]));
-
-    svg.selectAll("path")
-        .data(donutData)
-        .enter()
-        .append("path")
-        .attr("d", arc)
-        .style("fill", (d) => d[2]);
-
-    /*
-    Tick Marks
-    */
-
-    svg = d3.select(shadow.querySelector("#tickMarks"))
-        .append("svg")
-        .attr("width", width)
-        .attr("height", height)
-        .append("g")
-        .attr("transform", "translate("+ width/2 +","+ height/2 +")");
-
-    var tickMarksData = [[-0.1,0.1],
-        [24.9,25.1],
-        [49.9,50.1],
-        [74.9,75.1]];
-
-    var tMInnerRadius = 67;
-    var tMOuterRadius = 69;
-
-    var tMArc = d3.arc()
-        .innerRadius(tMInnerRadius)
-        .outerRadius(tMOuterRadius)
-        .startAngle((d) => percentScale(d[0]))
-        .endAngle((d) => percentScale(d[1]));
+    const arc = d3.arc()
+      .innerRadius(71)
+      .outerRadius(77)
+      .startAngle((d) => percentScale(d[0]))
+      .endAngle((d) => percentScale(d[1]));
 
     svg.selectAll("path")
-        .data(tickMarksData)
-        .enter()
-        .append("path")
-        .attr("d", tMArc)
-        .style("fill", "#808080");
+      .data(donutData)
+      .enter()
+      .append("path")
+      .attr("d", arc)
+      .style("fill", (d) => d[2]);
+    
+  }
+  
+  /*
+  Tick Marks
+  */
+  drawTickMarks(element,width,height){
 
-    /*
-    Area Graph
-    */
+    const percentScale = d3.scaleLinear()
+      .domain([0, 100])
+      .range([0, 2 * Math.PI]);
+
+    const tMInnerRadius = 67;
+    const tMOuterRadius = 69;
+    
+    const svg = d3.select(element)
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .append("g")
+      .attr("transform", "translate("+ width/2 +","+ height/2 +")");
+
+    const tickMarksData = [[-0.1,0.1],
+      [24.9,25.1],
+      [49.9,50.1],
+      [74.9,75.1]];
+
+    const tMArc = d3.arc()
+      .innerRadius(tMInnerRadius)
+      .outerRadius(tMOuterRadius)
+      .startAngle((d) => percentScale(d[0]))
+      .endAngle((d) => percentScale(d[1]));
+
+    svg.selectAll("path")
+      .data(tickMarksData)
+      .enter()
+      .append("path")
+      .attr("d", tMArc)
+      .style("fill", "#808080");
+  }
+  /*
+  Area Graph
+  */
+  drawAreaGraph(element,inputData,width,height){
+
+    const percentScale = d3.scaleLinear()
+      .domain([0, 100])
+      .range([0, 2 * Math.PI]);
 
     /*
     transforms following incoming json data
@@ -241,37 +262,33 @@ export class DonutTrio extends HTMLElement {
     [[0,24.800262527533583],[1,24.800262527533583]]
     TODO get rid of transformation by improving drawing procedure
     */
+    const areaChartData = inputData.data.map((item,i) => [i,item]);
 
-    let areaChartData = [];
-    for(let i=0;i<this._data.center.areaGraph.data.length;i++){
-      areaChartData.push([i,this._data.center.areaGraph.data[i]]);
-    }
-
-    svg = d3.select(shadow.querySelector("#areaGraph"))
-        .append("svg")
-        .attr("width", width)
-        .attr("height", height)
-        .append("g")
-        .attr("transform", "translate("+ width/2 +","+ height/2 +")");
+    const svg = d3.select(element)
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .append("g")
+      .attr("transform", "translate("+ width/2 +","+ height/2 +")");
 
     //circle mask applied to the area chart
     svg.append("clipPath")
-        .attr("id", "circleClip")
-        .append("ellipse")
-        .attr("cx", 0)          //x-centre
-        .attr("cy", 0)          //y-centre
-        .attr("rx", width/2-12) //x radius
-        .attr("ry", width/2-12);//y radius
+      .attr("id", "circleClip")
+      .append("ellipse")
+      .attr("cx", 0)          //x-centre
+      .attr("cy", 0)          //y-centre
+      .attr("rx", width/2-12) //x radius
+      .attr("ry", width/2-12);//y radius
 
     //100 item indexes used as x, scaled onto area's width
-    var x = d3.scaleLinear()
-        .domain([0,100])
-        .range([ -width/2, width/2 ]);
+    const x = d3.scaleLinear()
+      .domain([0,100])
+      .range([ -width/2, width/2 ]);
 
     //item values start at 25 and stay below 100, scaled onto area's height
-    var y = d3.scaleLinear()
-        .domain([0,100])
-        .range([ height/2,-height/2 ]);
+    const y = d3.scaleLinear()
+      .domain([0,100])
+      .range([ height/2,-height/2 ]);
 
     /*svg.append("g")
     .attr("transform", "translate(0," + height + ")")
@@ -279,18 +296,30 @@ export class DonutTrio extends HTMLElement {
         
     // Add the area
     svg.append("path")
-        .datum(areaChartData)
-        .attr("fill", this._data.center.areaGraph.areaColor)
-        .attr("fill-opacity", 0.1)
-        .attr("stroke", this._data.center.areaGraph.lineColor)
-        .attr("stroke-width", 1.5)
-        .attr("stroke-opacity", 0.3)
-        .attr("clip-path","url(#circleClip)")
-        .attr("d", d3.area()
-            .x((d) => x(d[0]))
-            .y0(y(0))
-            .y1((d) => y(d[1]))
-        )
+      .datum(areaChartData)
+      .attr("fill", inputData.areaColor)
+      .attr("fill-opacity", 0.1)
+      .attr("stroke", inputData.lineColor)
+      .attr("stroke-width", 1.5)
+      .attr("stroke-opacity", 0.3)
+      .attr("clip-path","url(#circleClip)")
+      .attr("d", d3.area()
+          .x((d) => x(d[0]))
+          .y0(y(0))
+          .y1((d) => y(d[1]))
+      )
 
+  }
+  //draws graphs using d3 library
+  drawGraphs(shadow,data){
+    /*
+    Initial SVG values
+    */
+    const width = 154;
+    const height = 154;
+
+    this.drawDonutGraph(shadow.querySelector("#donutGraph"),data.center.donut,width,height);
+    this.drawTickMarks(shadow.querySelector("#tickMarks"),width,height);
+    this.drawAreaGraph(shadow.querySelector("#areaGraph"),data.center.areaGraph,width,height);
   }
 }
